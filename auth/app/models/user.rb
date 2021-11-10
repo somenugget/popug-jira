1a479a26dbcb18ddb6ed3107be238638b4030408
Dmitriy Shcherbakan <some.nugget@gmail.com>
2021-11-10 09:39:19 +0200

    Devise for `task-tracker`

--- auth/app/models/user.rb
+++ auth/app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   include HasPublicId
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  has_many :access_grants,
+           class_name: 'Doorkeeper::AccessGrant',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
+  has_many :access_tokens,
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
 end
--- auth/config/application.rb
+++ auth/config/application.rb
@@ -5,7 +5,6 @@ require 'rails'
 require 'active_model/railtie'
 require 'active_record/railtie'
 require 'action_controller/railtie'
-require 'action_text/engine'
 require 'action_view/railtie'
 
 # Require the gems listed in Gemfile, including any gems
